779acc5009bfcf9bf1914b754760bce716052e46
Cooper Wright <cooper.wright@ons.gov.uk>
2025-12-18 12:06:25 +0000

    feat: deployed function to lambda

---
+++ terraform/service/eventbridge.tf
@@ -0,0 +1,33 @@
+module "eventbridge" {
+  source  = "terraform-aws-modules/eventbridge/aws"
+  version = "4.1.0"
+
+  role_name = "${var.lambda_name}-eventbridge-role"
+
+  create_bus = false
+
+  rules = {
+    "${var.lambda_name}-crons" = {
+      description         = "Trigger for ${var.lambda_name} Function"
+      schedule_expression = var.schedule
+    }
+  }
+
+  targets = {
+    "${var.lambda_name}-crons" = [
+      {
+        name  = "${var.lambda_name}-function-cron"
+        arn   = aws_lambda_function.lambda_function.arn
+        input = jsonencode({})
+      }
+    ]
+  }
+}
+
+resource "aws_lambda_permission" "allow_eventbridge_to_invoke_lambda" {
+  statement_id  = "AllowExecutionFromEventBridge"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda_function.arn
+  principal     = "events.amazonaws.com"
+  source_arn    = module.eventbridge.eventbridge_rules["${var.lambda_name}-crons"]["arn"]
+}
---
+++ terraform/service/variables.tf
@@ -0,0 +1,113 @@
+variable "aws_account_id" {
+  description = "AWS Account ID"
+  type        = string
+}
+
+variable "aws_access_key_id" {
+  description = "AWS Access Key ID"
+  type        = string
+}
+
+variable "aws_secret_access_key" {
+  description = "AWS Secret Access Key"
+  type        = string
+}
+
+variable "aws_secret_name" {
+  description = "The path to the AWS Secret Manager resource which contains the Github App .pem file"
+  type        = string
+}
+
+variable "aws_bucket_name" {
+  description = "The name of the S3 bucket which the cloud config is stored in. This should not include the environment name (i.e. sdp-dev) as it will be added automatically."
+  type        = string
+  default     = "digital-landscape"
+}
+
+variable "env_name" {
+  description = "AWS environment"
+  type        = string
+  default     = "sdp-dev"
+}
+
+variable "lambda_name" {
+  description = "AWS Lambda Function Name"
+  type        = string
+  default     = "lambda-function"
+}
+
+variable "lambda_version" {
+  description = "AWS Lambda Image Version"
+  type        = string
+  default     = "v0.0.1"
+}
+
+variable "lambda_arch" {
+  description = "AWS Lambda Architecture"
+  type        = string
+  default     = "x86_64"
+}
+
+variable "lambda_timeout" {
+  description = "AWS Lambda Timeout Value in Seconds"
+  type        = number
+  default     = 60
+}
+
+variable "schedule" {
+  description = "The schedule to trigger the lambda, rate(value minutes|hours|days) or cron(minutes hours day-of-month month day-of-week year)"
+  type        = string
+  default     = "cron(0 6 ? * 2 *)" // every Monday at 6am
+}
+
+variable "log_retention_days" {
+  description = "Lambda log retention in days"
+  type        = number
+  default     = 90
+}
+
+variable "github_org" {
+  description = "Github Organisation"
+  type        = string
+  default     = "ONS-Innovation"
+}
+
+variable "github_app_client_id" {
+  description = "Github App Client ID"
+  type        = string
+}
+
+variable "lambda_memory" {
+  description = "AWS Lambda Memory Size in MB"
+  type        = number
+  default     = 128
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-west-2"
+}
+
+variable "project_tag" {
+  description = "Project"
+  type        = string
+  default     = "SDP"
+}
+
+variable "team_owner_tag" {
+  description = "Team Owner"
+  type        = string
+  default     = "Knowledge Exchange Hub"
+}
+
+variable "business_owner_tag" {
+  description = "Business Owner"
+  type        = string
+  default     = "DST"
+}
+
+locals {
+  lambda_repo = "${var.env_name}-${var.lambda_name}"
+  bucket_name = "${var.env_name}-${var.aws_bucket_name}"
+}
