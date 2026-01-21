26d9f3dbb12a724c2a39025355058939bd40955c
Cooper Wright <cooper.wright@ons.gov.uk>
2026-01-21 13:28:17 +0000

    feat: update to Terraform to add error alarm

--- terraform/service/main.tf
+++ terraform/service/main.tf
@@ -138,3 +138,16 @@ resource "aws_cloudwatch_log_group" "loggroup" {
   name              = "/aws/lambda/${aws_lambda_function.lambda_function.function_name}"
   retention_in_days = var.log_retention_days
 }
+
+resource "aws_cloudwatch_metric_alarm" "error_alarm" {
+  alarm_name                = "Addressbook-Errors"
+  comparison_operator       = "GreaterThanThreshold"
+  evaluation_periods        = 1
+  metric_name               = "Errors"
+  namespace                 = "AWS/Lambda"
+  period                    = 300
+  statistic                 = "Sum"
+  threshold                 = 0
+  alarm_description         = "This metric counts the number of errors that the GitHub address book synchroniser Lambda function produces where if it is over 0 then an alarm is created"
+  insufficient_data_actions = []
+}
