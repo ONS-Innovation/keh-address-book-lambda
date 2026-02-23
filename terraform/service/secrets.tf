268a466f8a9d1073de4609a5b81953e1cc03b2f1
Hadi Qureshi <hadi.qureshi@ons.gov.uk>
2026-02-23 08:23:19 +0000

    update: fix secrets desc

--- terraform/service/secrets.tf
+++ terraform/service/secrets.tf
@@ -1,10 +1,10 @@
 # Secrets for rotated IAM user access keys
 resource "aws_secretsmanager_secret" "access_key" {
   name        = "${var.env_name}-${var.lambda_name}-access-key"
-  description = "Access Key ID for tech audit tool IAM user"
+  description = "Access Key ID for address book lambda IAM user"
 }
 
 resource "aws_secretsmanager_secret" "secret_key" {
   name        = "${var.env_name}-${var.lambda_name}-secret-key"
-  description = "Secret Access Key for tech audit tool IAM user"
+  description = "Secret Access Key for address book lambda IAM user"
 }
