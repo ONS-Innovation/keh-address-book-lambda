a7c335e001639b03d4b347182ed7cb77735106a9
Cooper Wright <cooper.wright@ons.gov.uk>
2026-07-03 10:00:43 +0100

    fix: added ecr_repository var to terraform infrastructure

--- concourse/scripts/terraform_infra.sh
+++ concourse/scripts/terraform_infra.sh
@@ -12,6 +12,7 @@ aws_secret_access_key=$(echo "$secrets" | jq -r .aws_secret_access_key)
 
 lambda_name=$(echo "$secrets" | jq -r .lambda_name)
 env_name=$(echo "$secrets" | jq -r .env_name)
+ecr_repository=$(echo "$secrets" | jq -r .ecr_repository)
 
 github_app_client_id=$(echo "$secrets" | jq -r .github_app_client_id)
 aws_secret_name=$(echo "$secrets" | jq -r .aws_secret_name)
@@ -47,4 +48,5 @@ terraform apply \
 	-var "github_org=$github_org" \
 	-var "aws_bucket_name=${aws_bucket_name}" \
 	-var "lambda_version=${tag}" \
+	-var "ecr_repository=${ecr_repository}" \
 	-auto-approve
